cc5f6bd35d0d6f4407fff4c4058246323fd65d7a
juliopari <jpari18@gmail.com>
2025-01-06 14:13:21 -0500

    initial

---
+++ prueba-concurrencia.bat
@@ -0,0 +1,5 @@
+start newman run reqres.postman_collection.json
+start newman run reqres.postman_collection.json
+start newman run reqres.postman_collection.json
+start newman run reqres.postman_collection.json
+start newman run reqres.postman_collection.json
